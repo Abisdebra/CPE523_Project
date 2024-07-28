0c5c434873fec211c13cf211cf1d71615b07fc4c
Abisdebra <abisoyetijani@gmail.com>
2024-07-28 19:55:28 +0100

    CDK code to create api gateway, lambda and dynamodb resources

--- lib/project-cdk-stack.ts
+++ lib/project-cdk-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { Table, AttributeType } from "aws-cdk-lib/aws-dynamodb";
+
+const path = require("path");
 
 export class ProjectCdkStack extends cdk.Stack {
+  public readonly backendFunction: Function;
+  public readonly restApi: LambdaRestApi;
+  public readonly productDetailsTable: Table;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.backendFunction = new Function(this, "BackendFunction", {
+      runtime: Runtime.PYTHON_3_12,
+      handler: "index.handler",
+      code: Code.fromAsset(path.join(__dirname, "../../Project-backend")),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProjectCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.restApi = new LambdaRestApi(this, "restAPI", {
+      handler: this.backendFunction,
+      proxy: false,
+    });
+
+    const postProductDetailsResource = this.restApi.root.addResource(
+      "post_product_details"
+    );
+    postProductDetailsResource.addMethod("POST");
+
+    const getProductDetailsResource = this.restApi.root.addResource(
+      "get_product_details"
+    );
+    getProductDetailsResource.addMethod("GET");
+
+    this.productDetailsTable = new Table(this, "ProductDetailsTable", {
+      tableName: "Product_Details_Table",
+      partitionKey: {
+        name: "product_id",
+        type: AttributeType.STRING,
+      },
+    });
+
+    this.productDetailsTable.grantReadWriteData(this.backendFunction);
   }
 }
